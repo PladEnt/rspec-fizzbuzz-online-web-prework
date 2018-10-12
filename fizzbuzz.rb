a613a97829c44217e18de4d1da1f47f3f6c5d7ce
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-10-12 13:24:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@ def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
   end
-  elsif int % 3 == 0
+  if int % 3 == 0
     return "Fizz"
   end
-  elsif int % 5 == 0
+  if int % 5 == 0
     return "Buzz"
   end
   
